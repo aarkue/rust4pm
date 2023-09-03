8d45e9080b0b46bd0591855f983f73fe15c16669
aarkue <mail@aarkue.eu>
2023-09-03 16:42:35 +0200

    Python: Test performance of log passing options

--- python_bridge/src/lib.rs
+++ python_bridge/src/lib.rs
@@ -1,9 +1,26 @@
 use std::collections::HashMap;
+use std::time::Instant;
 
-use pyo3::prelude::*;
-use pyo3::Python;
+use pm_rust::add_start_end_acts;
+use pm_rust::export_log_to_byte_vec;
+use pm_rust::export_log_to_string;
+use pm_rust::import_log_from_byte_array;
+use pm_rust::import_log_from_str;
+use pm_rust::EventLog;
+use pm_rust::EventLogActivityProjection;
+use pm_rust::Trace;
+use pm_rust::TRACE_ID_NAME;
 use pm_rust::{loop_sum_sqrt, Event};
+use polars::prelude::DataFrame;
+use pyo3::prelude::*;
+use pyo3::types::PyBytes;
+use pyo3::Python;
+use pyo3_polars::PyDataFrame;
+use pythonize::depythonize;
 use pythonize::pythonize;
+use rayon::prelude::IntoParallelRefIterator;
+use rayon::prelude::ParallelIterator;
+
 #[pyclass]
 #[derive(Debug, Default, Clone)]
 pub struct PyBridgeAttributes {
@@ -168,19 +185,143 @@ impl From<EventLog> for PyBridgeEventLog {
         }
     }
 }
+
 #[pyfunction]
 fn get_event(act: String) -> pyo3::Py<PyAny> {
     let ev = Event::new(act);
-     Python::with_gil(|py| {
-        let obj: Py<PyAny> = pythonize(py,&ev).unwrap();
+    Python::with_gil(|py| {
+        let obj: Py<PyAny> = pythonize(py, &ev).unwrap();
         obj
     })
 }
 
 #[pyfunction]
-fn get_result_map(a: usize, b: usize) -> PyResult<HashMap<String,f32>>{
-    let mut map : HashMap<String,f32> = HashMap::new();
-    map.insert("Result".into(), loop_sum_sqrt(a,b));
+fn test_bridge_log(pylog: PyBridgeEventLog) -> PyResult<PyBridgeEventLog> {
+    let mut log: EventLog = pylog.into();
+    add_start_end_acts(&mut log);
+    let export_log: PyBridgeEventLog = log.into();
+    println!("Added start/end acts!");
+    Ok(export_log)
+}
+
+#[pyfunction]
+fn polars_df_to_log(pydf: PyDataFrame) -> PyResult<PyBridgeEventLog> {
+    let mut df: DataFrame = pydf.into();
+    df.as_single_chunk_par();
+    let groups = df.partition_by_stable([TRACE_ID_NAME], true).unwrap();
+    let columns = df.get_column_names();
+    let mut log = PyBridgeEventLog {
+        attributes: PyBridgeAttributes::default(),
+        traces: vec![],
+    };
+
+    let traces: Vec<PyBridgeTrace> = groups
+        .par_iter()
+        .map(|g| {
+            let events: Vec<PyBridgeEvent> = (0..g.height())
+                .into_iter()
+                .map(|i| {
+                    let val = g.get_row(i).unwrap().0;
+                    let attributes: HashMap<String, String> = columns
+                        .iter()
+                        .zip(val.iter())
+                        .map(|(c, v)| {
+                            return (c.to_string(), format!("{:?}", v));
+                        })
+                        .collect();
+                    PyBridgeEvent {
+                        attributes: attributes.into(),
+                    }
+                })
+                .collect();
+            let mut trace = PyBridgeTrace::new(
+                events[0]
+                    .attributes
+                    .attributes
+                    .get(TRACE_ID_NAME)
+                    .unwrap()
+                    .clone(),
+            );
+            trace.events = events;
+            return trace;
+        })
+        .collect();
+    log.traces = traces;
+    Ok(log)
+}
+
+#[pyfunction]
+fn test_event_log(py: Python<'_>, log_py: Py<PyAny>) -> PyResult<Py<PyAny>> {
+    let mut now = Instant::now();
+    let mut log: EventLog = depythonize(log_py.as_ref(py)).unwrap();
+    println!("Time until struct ready: {:.2?}", now.elapsed());
+    now = Instant::now();
+    log.attributes.insert(
+        "name".to_string(),
+        "Transformed Rust Log from byte[]".into(),
+    );
+    println!("Time until into EventLog: {:.2?}", now.elapsed());
+    now = Instant::now();
+    add_start_end_acts(&mut log);
+    println!("Time until start/end added: {:.2?}", now.elapsed());
+    now = Instant::now();
+    let log_projection: EventLogActivityProjection<usize> = log.into();
+    let log_again: EventLog = log_projection.into();
+    println!("Time until into/from: {:.2?}", now.elapsed());
+    now = Instant::now();
+    let x: Py<PyAny> = pythonize(py, &log_again).unwrap();
+    println!("Export to python: {:.2?}", now.elapsed());
+    Ok(x)
+}
+
+#[pyfunction]
+fn test_event_log_str(log: String) -> PyResult<String> {
+    let mut log: EventLog = import_log_from_str(log);
+    //  Python::with_gil(|py| {
+    //     let mut log: EventLog = depythonize(log.as_ref(py)).unwrap();
+    add_start_end_acts(&mut log);
+    //     // let log_projection: EventLogActivityProjection<usize> = log.into();
+    //     // println!("Projection with activities {:?}",log_projection.activities);
+    //     // let log: EventLog = log_projection.into();
+    //     let back: Py<PyAny> = pythonize(py,&log).unwrap();
+    //     back
+    Ok(export_log_to_string(&log))
+    // })
+}
+
+#[pyfunction]
+fn test_event_log_bytes(py: Python<'_>, log_bytes: Py<PyBytes>) -> PyResult<&PyBytes> {
+    let mut now = Instant::now();
+    let bytes: &[u8] = log_bytes.extract(py).unwrap();
+    println!("Got {:?} bytes in {:.2?}", bytes.len(), now.elapsed());
+    now = Instant::now();
+    let mut log: EventLog = import_log_from_byte_array(&bytes);
+
+    println!("Time until struct ready: {:.2?}", now.elapsed());
+    now = Instant::now();
+    log.attributes.insert(
+        "name".to_string(),
+        "Transformed Rust Log from byte[]".into(),
+    );
+    println!("Time until into EventLog: {:.2?}", now.elapsed());
+    now = Instant::now();
+    add_start_end_acts(&mut log);
+    println!("Time until start/end added: {:.2?}", now.elapsed());
+    now = Instant::now();
+    let log_projection: EventLogActivityProjection<usize> = log.into();
+    let log_again: EventLog = log_projection.into();
+    println!("Time until into/from: {:.2?}", now.elapsed());
+    now = Instant::now();
+    let export_vec = export_log_to_byte_vec(&log_again);
+    println!("ExportVec to byte array: {:.2?}", now.elapsed());
+    let py_bytes = PyBytes::new(py, &export_vec);
+    Ok(py_bytes)
+}
+
+#[pyfunction]
+fn get_result_map(a: usize, b: usize) -> PyResult<HashMap<String, f32>> {
+    let mut map: HashMap<String, f32> = HashMap::new();
+    map.insert("Result".into(), loop_sum_sqrt(a, b));
     Ok(map)
 }
 
@@ -188,5 +329,13 @@ fn get_result_map(a: usize, b: usize) -> PyResult<HashMap<String,f32>>{
 fn rust_bridge_pm_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(get_event, m)?)?;
     m.add_function(wrap_pyfunction!(get_result_map, m)?)?;
+    m.add_function(wrap_pyfunction!(test_event_log, m)?)?;
+    m.add_function(wrap_pyfunction!(test_event_log_str, m)?)?;
+    m.add_function(wrap_pyfunction!(test_event_log_bytes, m)?)?;
+    m.add_function(wrap_pyfunction!(polars_df_to_log, m)?)?;
+    m.add_function(wrap_pyfunction!(test_bridge_log, m)?)?;
+    m.add_class::<PyBridgeEvent>()?;
+    m.add_class::<PyBridgeTrace>()?;
+    m.add_class::<PyBridgeEventLog>()?;
     Ok(())
 }
