39c7897e64689172d968a1c566582e4a00191c6c
aarkue <mail@aarkue.eu>
2023-09-08 10:35:31 +0200

    Some samples/tests in footbridge

--- footbridge/src/main.rs
+++ footbridge/src/main.rs
@@ -1,6 +1,58 @@
-use pm_rust;
+use std::vec;
+
+use pm_rust::{
+    xes::import_xes::import_log_xes, Attribute, AttributeAddable, AttributeValue, Attributes,
+    DateTime, Utc, Uuid,
+};
 fn main() {
-    println!("Hello, world!");
-    let res = pm_rust::loop_sum_sqrt(4,5);
-    println!("{}",res);
+    let mut attributes = Attributes::new();
+    attributes.add_to_attributes("test".into(), AttributeValue::String("Hello".into()));
+
+    attributes.add_to_attributes(
+        "date-test".into(),
+        AttributeValue::Date(DateTime::<Utc>::default()),
+    );
+
+    attributes.add_to_attributes("int-test".into(), AttributeValue::Int(42));
+
+    attributes.add_to_attributes(
+        "date-test".into(),
+        AttributeValue::Date(DateTime::<Utc>::default()),
+    );
+    attributes.add_to_attributes("float-test".into(), AttributeValue::Float(1.337));
+    attributes.add_to_attributes("boolean-test".into(), AttributeValue::Boolean(true));
+    attributes.add_to_attributes("id-test".into(), AttributeValue::ID(Uuid::new_v4()));
+    attributes.add_to_attributes(
+        "list-test".into(),
+        AttributeValue::List(vec![
+            Attribute {
+                key: "first".into(),
+                value: AttributeValue::Int(1),
+            },
+            Attribute {
+                key: "first".into(),
+                value: AttributeValue::Float(1.1),
+            },
+            Attribute {
+                key: "second".into(),
+                value: AttributeValue::Int(2),
+            },
+        ]),
+    );
+
+    let mut container_test_inner = Attributes::new();
+    container_test_inner.add_to_attributes("first".into(), AttributeValue::Int(1));
+    container_test_inner.add_to_attributes("second".into(), AttributeValue::Int(2));
+    container_test_inner.add_to_attributes("third".into(), AttributeValue::Int(3));
+    attributes.add_to_attributes(
+        "container-test".into(),
+        AttributeValue::Container(container_test_inner),
+    );
+    // let event: Event = Event { attributes };
+    // println!("Hello, world!");
+    // let json = serde_json::to_string_pretty(&event.attributes).unwrap();
+    // println!("{}", json);
+
+    let log =
+        import_log_xes(&"/home/aarkue/dow/event_logs/BPI_Challenge_2020_request_for_payments.xes");
 }
