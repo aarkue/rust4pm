828f1ee414c4698e9d33c1f6b0f857e98b0a3009
aarkue <mail@aarkue.eu>
2023-09-11 16:47:21 +0200

    Rework of PetriNet: ID-based

--- pm_rust/src/petri_net/petri_net_struct.rs
+++ pm_rust/src/petri_net/petri_net_struct.rs
@@ -12,10 +12,10 @@ struct Transition {
 }
 
 #[derive(Debug)]
-enum PetriNetNodes<'a> {
+enum PetriNetNodes {
     None,
-    Places(Vec<&'a Place>),
-    Transitions(Vec<&'a Transition>),
+    Places(Vec<PlaceID>),
+    Transitions(Vec<TransitionID>),
 }
 
 #[derive(Debug)]
@@ -24,10 +24,35 @@ enum ArcType {
     TransitionPlace(Uuid, Uuid),
 }
 
+impl ArcType {
+  fn place_to_transition(from: PlaceID, to: TransitionID) -> ArcType{
+    return ArcType::PlaceTransition(from.0, to.0)
+  }
+  fn transition_to_place(from: TransitionID, to: PlaceID) -> ArcType{
+    return ArcType::TransitionPlace(from.0, to.0)
+  }
+}
+
 #[derive(Debug)]
 struct Arc {
     from_to: ArcType,
     weight: u32,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+struct PlaceID(Uuid);
+impl From<&Place> for PlaceID {
+    fn from(value: &Place) -> Self {
+        PlaceID(value.id)
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+struct TransitionID(Uuid);
+impl From<&Transition> for TransitionID {
+    fn from(value: &Transition) -> Self {
+        TransitionID(value.id)
+    }
 }
 
 #[derive(Debug)]
@@ -45,21 +70,21 @@ impl PetriNet {
             arcs: Vec::new(),
         }
     }
-    fn add_place(&mut self, place_id: Option<Uuid>) -> Place {
+    fn add_place(&mut self, place_id: Option<Uuid>) -> PlaceID {
         let place_id = place_id.unwrap_or(Uuid::new_v4());
         let place = Place { id: place_id };
-        self.places.insert(place_id, place.clone());
-        return place;
+        self.places.insert(place_id, place);
+        return PlaceID(place_id)
     }
 
-    fn add_transition(&mut self, label: Option<String>, transition_id: Option<Uuid>) -> Transition {
+    fn add_transition(&mut self, label: Option<String>, transition_id: Option<Uuid>) -> TransitionID {
         let transition_id = transition_id.unwrap_or(Uuid::new_v4());
         let transition = Transition {
             id: transition_id,
             label,
         };
-        self.transitions.insert(transition_id, transition.clone());
-        return transition;
+        self.transitions.insert(transition_id, transition);
+        return TransitionID(transition_id)
     }
     fn add_arc(&mut self, from_to: ArcType, weight: Option<u32>) {
         self.arcs.push(Arc {
@@ -71,32 +96,62 @@ impl PetriNet {
     fn preset_of(&self, id: Uuid) -> PetriNetNodes {
         if self.places.contains_key(&id) {
             let p = self.places.get(&id).unwrap();
-            PetriNetNodes::Transitions(self.preset_of_place(p))
+            PetriNetNodes::Transitions(self.preset_of_place(p.into()))
         } else if self.transitions.contains_key(&id) {
             let t = self.transitions.get(&id).unwrap();
-            PetriNetNodes::Places(self.preset_of_transition(t))
+            PetriNetNodes::Places(self.preset_of_transition(t.into()))
         } else {
             PetriNetNodes::None
         }
     }
 
-    fn preset_of_place(&self, p: &Place) -> Vec<&Transition> {
+    fn preset_of_place(&self, p: PlaceID) -> Vec<TransitionID> {
         self.arcs
             .iter()
             .filter_map(|x: &Arc| match x.from_to {
-                ArcType::PlaceTransition(from, to) if from == p.id => self.transitions.get(&to),
-                ArcType::TransitionPlace(from, to) if to == p.id => self.transitions.get(&from),
+                ArcType::TransitionPlace(from, to) if to == p.0 =>Some(TransitionID(from)),
                 _ => None,
             })
             .collect()
     }
 
-    fn preset_of_transition(&self, t: &Transition) -> Vec<&Place> {
+    fn preset_of_transition(&self, t: TransitionID) -> Vec<PlaceID> {
         self.arcs
             .iter()
             .filter_map(|x: &Arc| match x.from_to {
-                ArcType::PlaceTransition(from, to) if from == t.id => self.places.get(&to),
-                ArcType::TransitionPlace(from, to) if to == t.id => self.places.get(&from),
+                ArcType::PlaceTransition(from, to) if to == t.0 => Some(PlaceID(from)),
+                _ => None,
+            })
+            .collect()
+    }
+ 
+    fn postset_of(&self, id: Uuid) -> PetriNetNodes {
+      if self.places.contains_key(&id) {
+          let p = self.places.get(&id).unwrap();
+          PetriNetNodes::Transitions(self.postset_of_place(p.into()))
+      } else if self.transitions.contains_key(&id) {
+          let t = self.transitions.get(&id).unwrap();
+          PetriNetNodes::Places(self.postset_of_transition(t.into()))
+      } else {
+          PetriNetNodes::None
+      }
+  }
+
+    fn postset_of_place(&self, p: PlaceID) -> Vec<TransitionID> {
+        self.arcs
+            .iter()
+            .filter_map(|x: &Arc| match x.from_to {
+                ArcType::PlaceTransition(from, to) if from == p.0 => Some(TransitionID(to)),
+                _ => None,
+            })
+            .collect()
+    }
+
+    fn postset_of_transition(&self, t: TransitionID) -> Vec<PlaceID> {
+        self.arcs
+            .iter()
+            .filter_map(|x: &Arc| match x.from_to {
+                ArcType::TransitionPlace(from, to) if from == t.0 => Some(PlaceID(to)),
                 _ => None,
             })
             .collect()
@@ -113,12 +168,14 @@ mod tests {
         let p1 = net.add_place(None);
         let t1 = net.add_transition(Some("Have fun".into()), None);
         let t2 = net.add_transition(Some("Sleep".into()), None);
-        net.add_arc(ArcType::PlaceTransition(p1.id, t1.id), None);
+        net.add_arc(ArcType::place_to_transition(p1, t1), None);
+        net.add_arc(ArcType::transition_to_place(t2, p1), None);
         println!("Constructed petri net: {:?}", net);
 
-        assert!(net.preset_of_transition(&t1).is_empty());
-        assert!(net.preset_of_place(&p1) == vec![&t1]);
-        assert!(net.preset_of_place(&p1) != vec![&t2]);
-        assert!(net.preset_of_transition(&t2).is_empty());
+        assert!(net.postset_of_transition(t1).is_empty());
+        assert!(net.preset_of_transition(t1) == vec![p1]);
+        assert!(net.postset_of_place(p1) == vec![t1]);
+        assert!(net.preset_of_place(p1) == vec![t2]);
+        assert!(net.preset_of_transition(t2).is_empty());
     }
 }
