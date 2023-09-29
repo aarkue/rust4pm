b85a31e4e96b2115a83ca8e50de668deb714fca3
aarkue <mail@aarkue.eu>
2023-09-29 13:41:06 +0200

    Implemented Java Petri Net Bridges

--- java_side/java_side_gson/RustBridgeJavaGson/src/org/processmining/alpharevisitexperiments/bridge/HelloProcessMining.java
+++ java_side/java_side_gson/RustBridgeJavaGson/src/org/processmining/alpharevisitexperiments/bridge/HelloProcessMining.java
@@ -1,15 +1,23 @@
 package org.processmining.alpharevisitexperiments.bridge;
 
 import com.google.common.collect.Sets;
-import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapter;
+import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import org.deckfour.xes.id.XID;
 import org.deckfour.xes.model.*;
 import org.deckfour.xes.model.impl.*;
+import org.processmining.acceptingpetrinet.models.AcceptingPetriNet;
+import org.processmining.acceptingpetrinet.models.impl.AcceptingPetriNetImpl;
+import org.processmining.models.graphbased.NodeID;
+import org.processmining.models.graphbased.directed.petrinet.Petrinet;
+import org.processmining.models.graphbased.directed.petrinet.elements.Place;
+import org.processmining.models.graphbased.directed.petrinet.elements.Transition;
+import org.processmining.models.graphbased.directed.petrinet.impl.PetrinetFactory;
+import org.processmining.models.semantics.petrinet.Marking;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
@@ -66,6 +74,8 @@ public class HelloProcessMining {
     private static native String getCompleteRustTraceAsStringJsonCompatible(long eventLogPointer, int index);
 
     private static native String getCompleteRustLogAsStringJsonCompatible(long eventLogPointer);
+
+    private static native String addSampleDisconnectedNet(String petrinet);
 
 
     private static void addAllAttributesFromTo(XAttributeMap from, Map<String, String> to) {
@@ -243,114 +253,132 @@ public class HelloProcessMining {
         return newLog;
     }
 
+    private static String extractNodeID(NodeID nodeID) {
+        return nodeID.toString().substring("node ".length());
+    }
+
+    public static AcceptingPetriNet bridgePetriNetFromWrapper(PetriNetBridge pnb) {
+        Petrinet pn = PetrinetFactory.newPetrinet("Petri net");
+        HashMap<String, Transition> transitionMap = new HashMap<>();
+        HashMap<String, Place> placeMap = new HashMap<>();
+        pnb.transitions.values().stream().forEach(t -> {
+            Transition trans = pn.addTransition(t.label);
+            if (t.label == null) {
+                trans.setInvisible(true);
+            }
+            transitionMap.put(t.id, trans);
+        });
+        pnb.places.values().stream().forEach(p -> {
+            placeMap.put(p.id, pn.addPlace(p.id));
+        });
+        pnb.arcs.forEach(arc -> {
+            if (FromToBridge.PlaceTransition.equals(arc.from_to.type)) {
+                pn.addArc(placeMap.get(arc.from_to.nodes[0]), transitionMap.get(arc.from_to.nodes[1]));
+            } else {
+                pn.addArc(transitionMap.get(arc.from_to.nodes[0]), placeMap.get(arc.from_to.nodes[1]));
+
+            }
+        });
+        AcceptingPetriNet apn = new AcceptingPetriNetImpl(pn);
+        if (pnb.initial_marking != null) {
+            Marking im = new Marking();
+            pnb.initial_marking.forEach((p, n) -> {
+                for (int i = 0; i < n; i++) {
+                    im.add(placeMap.get(p));
+                }
+            });
+            apn.setInitialMarking(im);
+        }
+        if (pnb.final_markings != null) {
+            apn.setFinalMarkings(new HashSet<>());
+            Marking newFinalMarking = new Marking();
+            for (HashMap<String, Integer> m : pnb.final_markings) {
+                m.forEach((p, n) -> {
+                    for (int i = 0; i < n; i++) {
+                        newFinalMarking.add(placeMap.get(p));
+                    }
+                });
+                apn.getFinalMarkings().add(newFinalMarking);
+            }
+        }
+        return apn;
+    }
+
+    public static PetriNetBridge bridgePetriNetToWrapper(Petrinet net) {
+        return bridgePetriNetToWrapper(net, null, null);
+    }
+
+    public static PetriNetBridge bridgePetriNetToWrapper(AcceptingPetriNet apn) {
+        return bridgePetriNetToWrapper(apn.getNet(), apn.getInitialMarking(), apn.getFinalMarkings());
+    }
+
+    public static PetriNetBridge bridgePetriNetToWrapper(Petrinet net, Marking initialMarking, Set<Marking> finalMarkings) {
+        PetriNetBridge pnb = new PetriNetBridge();
+
+        for (org.processmining.models.graphbased.directed.petrinet.elements.Transition t : net.getTransitions()) {
+            TransitionBridge tb = new TransitionBridge();
+            tb.id = extractNodeID(t.getId());
+            if (!t.isInvisible()) {
+                tb.label = t.getLabel();
+            }
+            pnb.transitions.put(tb.id, tb);
+        }
+
+        for (org.processmining.models.graphbased.directed.petrinet.elements.Place t : net.getPlaces()) {
+            PlaceBridge pb = new PlaceBridge();
+            pb.id = extractNodeID(t.getId());
+            pnb.places.put(pb.id, pb);
+        }
+
+        for (org.processmining.models.graphbased.directed.petrinet.PetrinetEdge<? extends org.processmining.models.graphbased.directed.petrinet.PetrinetNode, ? extends org.processmining.models.graphbased.directed.petrinet.PetrinetNode> e : net.getEdges()) {
+            String sourceID = extractNodeID(e.getSource().getId());
+            String targetID = extractNodeID(e.getTarget().getId());
+            ArcBridge ab = new ArcBridge();
+            ab.from_to.nodes = new String[]{sourceID, targetID};
+            if (e.getSource() instanceof Place) {
+                ab.from_to.type = FromToBridge.PlaceTransition;
+            } else {
+                ab.from_to.type = FromToBridge.TransitionPlace;
+            }
+            pnb.arcs.add(ab);
+        }
+        if (initialMarking != null) {
+            for (Place p : initialMarking) {
+                String placeID = extractNodeID(p.getId());
+                pnb.initial_marking.put(placeID, pnb.initial_marking.getOrDefault(placeID, 0) + 1);
+            }
+        }
+        if (finalMarkings != null) {
+            for (Marking m : finalMarkings) {
+                HashMap<String, Integer> finalMarking = new HashMap<>();
+                for (Place p : m) {
+                    String placeID = extractNodeID(p.getId());
+                    finalMarking.put(placeID, finalMarking.getOrDefault(placeID, 0) + 1);
+                }
+                pnb.final_markings.add(finalMarking);
+            }
+        }
+        return pnb;
+    }
+
+    public static AcceptingPetriNet javaPetriNetToRust(AcceptingPetriNet net) {
+        String n = gson.toJson(bridgePetriNetToWrapper(net));
+        String resJSON = addSampleDisconnectedNet(n);
+        PetriNetBridge bridgeNet = gson.fromJson(resJSON, PetriNetBridge.class);
+        return bridgePetriNetFromWrapper(bridgeNet);
+    }
+
     public static XLog test(XLog l) {
         System.out.println("Start parallel Rust test on log with size " + l.size());
         return createRustEventLogHelperPar(l);
     }
 
+    public static AcceptingPetriNet testNet(AcceptingPetriNet n) {
+        return javaPetriNetToRust(n);
+    }
+
     public static void main(String[] args) {
-        String json = "{\n" +
-                "  \"test\": {\n" +
-                "    \"key\": \"test\",\n" +
-                "    \"value\": {\n" +
-                "      \"type\": \"String\",\n" +
-                "      \"content\": \"Hello\"\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"boolean-test\": {\n" +
-                "    \"key\": \"boolean-test\",\n" +
-                "    \"value\": {\n" +
-                "      \"type\": \"Boolean\",\n" +
-                "      \"content\": true\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"date-test\": {\n" +
-                "    \"key\": \"date-test\",\n" +
-                "    \"value\": {\n" +
-                "      \"type\": \"Date\",\n" +
-                "      \"content\": 0\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"container-test\": {\n" +
-                "    \"key\": \"container-test\",\n" +
-                "    \"value\": {\n" +
-                "      \"type\": \"Container\",\n" +
-                "      \"content\": {\n" +
-                "        \"first\": {\n" +
-                "          \"key\": \"first\",\n" +
-                "          \"value\": {\n" +
-                "            \"type\": \"Int\",\n" +
-                "            \"content\": 1\n" +
-                "          }\n" +
-                "        },\n" +
-                "        \"second\": {\n" +
-                "          \"key\": \"second\",\n" +
-                "          \"value\": {\n" +
-                "            \"type\": \"Int\",\n" +
-                "            \"content\": 2\n" +
-                "          }\n" +
-                "        },\n" +
-                "        \"third\": {\n" +
-                "          \"key\": \"third\",\n" +
-                "          \"value\": {\n" +
-                "            \"type\": \"Int\",\n" +
-                "            \"content\": 3\n" +
-                "          }\n" +
-                "        }\n" +
-                "      }\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"float-test\": {\n" +
-                "    \"key\": \"float-test\",\n" +
-                "    \"value\": {\n" +
-                "      \"type\": \"Float\",\n" +
-                "      \"content\": 1.337\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"list-test\": {\n" +
-                "    \"key\": \"list-test\",\n" +
-                "    \"value\": {\n" +
-                "      \"type\": \"List\",\n" +
-                "      \"content\": [\n" +
-                "        {\n" +
-                "          \"key\": \"first\",\n" +
-                "          \"value\": {\n" +
-                "            \"type\": \"Int\",\n" +
-                "            \"content\": 1\n" +
-                "          }\n" +
-                "        },\n" +
-                "        {\n" +
-                "          \"key\": \"first\",\n" +
-                "          \"value\": {\n" +
-                "            \"type\": \"Float\",\n" +
-                "            \"content\": 1.1\n" +
-                "          }\n" +
-                "        },\n" +
-                "        {\n" +
-                "          \"key\": \"second\",\n" +
-                "          \"value\": {\n" +
-                "            \"type\": \"Int\",\n" +
-                "            \"content\": 2\n" +
-                "          }\n" +
-                "        }\n" +
-                "      ]\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"int-test\": {\n" +
-                "    \"key\": \"int-test\",\n" +
-                "    \"value\": {\n" +
-                "      \"type\": \"Int\",\n" +
-                "      \"content\": 42\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"id-test\": {\n" +
-                "    \"key\": \"id-test\",\n" +
-                "    \"value\": {\n" +
-                "      \"type\": \"ID\",\n" +
-                "      \"content\": \"6f535ff1-5523-43f3-9e06-a61394c356ec\"\n" +
-                "    }\n" +
-                "  }\n" +
-                "}";
+        String json = "{\n" + "  \"test\": {\n" + "    \"key\": \"test\",\n" + "    \"value\": {\n" + "      \"type\": \"String\",\n" + "      \"content\": \"Hello\"\n" + "    }\n" + "  },\n" + "  \"boolean-test\": {\n" + "    \"key\": \"boolean-test\",\n" + "    \"value\": {\n" + "      \"type\": \"Boolean\",\n" + "      \"content\": true\n" + "    }\n" + "  },\n" + "  \"date-test\": {\n" + "    \"key\": \"date-test\",\n" + "    \"value\": {\n" + "      \"type\": \"Date\",\n" + "      \"content\": 0\n" + "    }\n" + "  },\n" + "  \"container-test\": {\n" + "    \"key\": \"container-test\",\n" + "    \"value\": {\n" + "      \"type\": \"Container\",\n" + "      \"content\": {\n" + "        \"first\": {\n" + "          \"key\": \"first\",\n" + "          \"value\": {\n" + "            \"type\": \"Int\",\n" + "            \"content\": 1\n" + "          }\n" + "        },\n" + "        \"second\": {\n" + "          \"key\": \"second\",\n" + "          \"value\": {\n" + "            \"type\": \"Int\",\n" + "            \"content\": 2\n" + "          }\n" + "        },\n" + "        \"third\": {\n" + "          \"key\": \"third\",\n" + "          \"value\": {\n" + "            \"type\": \"Int\",\n" + "            \"content\": 3\n" + "          }\n" + "        }\n" + "      }\n" + "    }\n" + "  },\n" + "  \"float-test\": {\n" + "    \"key\": \"float-test\",\n" + "    \"value\": {\n" + "      \"type\": \"Float\",\n" + "      \"content\": 1.337\n" + "    }\n" + "  },\n" + "  \"list-test\": {\n" + "    \"key\": \"list-test\",\n" + "    \"value\": {\n" + "      \"type\": \"List\",\n" + "      \"content\": [\n" + "        {\n" + "          \"key\": \"first\",\n" + "          \"value\": {\n" + "            \"type\": \"Int\",\n" + "            \"content\": 1\n" + "          }\n" + "        },\n" + "        {\n" + "          \"key\": \"first\",\n" + "          \"value\": {\n" + "            \"type\": \"Float\",\n" + "            \"content\": 1.1\n" + "          }\n" + "        },\n" + "        {\n" + "          \"key\": \"second\",\n" + "          \"value\": {\n" + "            \"type\": \"Int\",\n" + "            \"content\": 2\n" + "          }\n" + "        }\n" + "      ]\n" + "    }\n" + "  },\n" + "  \"int-test\": {\n" + "    \"key\": \"int-test\",\n" + "    \"value\": {\n" + "      \"type\": \"Int\",\n" + "      \"content\": 42\n" + "    }\n" + "  },\n" + "  \"id-test\": {\n" + "    \"key\": \"id-test\",\n" + "    \"value\": {\n" + "      \"type\": \"ID\",\n" + "      \"content\": \"6f535ff1-5523-43f3-9e06-a61394c356ec\"\n" + "    }\n" + "  }\n" + "}";
 //        XAttributeMap attributeMap = gson.fromJson(json, xAttributeMap);
 //        System.out.println(attributeMap);
 //        String backToJson = gson.toJson(attributeMap, xAttributeMap);
@@ -383,6 +411,70 @@ public class HelloProcessMining {
         return log;
     }
 
+    private enum AttributeType {
+        String("String"), Date("Date"), Int("Int"), Float("Float"), Boolean("Boolean"), ID("ID"), List("List"), Container("Container"), None("None");
+        private String type;
+
+        AttributeType(String type) {
+            this.type = type;
+        }
+
+        @Override
+        public String toString() {
+            return type;
+        }
+
+        public String getAttributeType() {
+            return type;
+        }
+    }
+
+    static class PlaceBridge {
+        public String id;
+    }
+
+    static class TransitionBridge {
+        public String id;
+        public String label;
+
+        public String getId() {
+            return id;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+    }
+
+    static class FromToBridge {
+        static String PlaceTransition = "PlaceTransition";
+        static String TransitionPlace = "TransitionPlace";
+        public String type;
+        public String[] nodes;
+    }
+
+    static class ArcBridge {
+        public FromToBridge from_to = new FromToBridge();
+        public int weight = 1;
+    }
+
+    static class PetriNetBridge {
+        public HashMap<String, PlaceBridge> places = new HashMap<>();
+        public HashMap<String, TransitionBridge> transitions = new HashMap<>();
+        public ArrayList<ArcBridge> arcs = new ArrayList<>();
+        public HashMap<String, Integer> initial_marking = new HashMap<>();
+        public List<HashMap<String, Integer>> final_markings = new ArrayList<>();
+
+
+        public HashMap<String, PlaceBridge> getPlaces() {
+            return places;
+        }
+
+        public HashMap<String, TransitionBridge> getTransitions() {
+            return transitions;
+        }
+
+    }
 
     private static class XEventTypeAdapter extends TypeAdapter<XEvent> {
         @Override
@@ -456,24 +548,6 @@ public class HelloProcessMining {
         }
     }
 
-    private enum AttributeType {
-        String("String"), Date("Date"), Int("Int"), Float("Float"), Boolean("Boolean"), ID("ID"), List("List"), Container("Container"), None("None");
-        private String type;
-
-        AttributeType(String type) {
-            this.type = type;
-        }
-
-        @Override
-        public String toString() {
-            return type;
-        }
-
-        public String getAttributeType() {
-            return type;
-        }
-    }
-
     private static class XAttributeTypeAdapter extends TypeAdapter<XAttribute> {
         @Override
         public void write(JsonWriter out, XAttribute value) throws IOException {
@@ -486,31 +560,31 @@ public class HelloProcessMining {
                 out.value(AttributeType.Date.type);
                 out.name("content");
                 out.value(((XAttributeTimestamp) value).getValue().getTime());
-            } else if (value instanceof XAttributeDiscrete){
+            } else if (value instanceof XAttributeDiscrete) {
                 out.value(AttributeType.Int.type);
                 out.name("content");
                 out.value(((XAttributeDiscrete) value).getValue());
-            }else if (value instanceof XAttributeContinuous){
+            } else if (value instanceof XAttributeContinuous) {
                 out.value(AttributeType.Float.type);
                 out.name("content");
                 out.value(((XAttributeContinuous) value).getValue());
-            }else if (value instanceof XAttributeBoolean){
+            } else if (value instanceof XAttributeBoolean) {
                 out.value(AttributeType.Boolean.type);
                 out.name("content");
                 out.value(((XAttributeBoolean) value).getValue());
-            }else if (value instanceof XAttributeID){
+            } else if (value instanceof XAttributeID) {
                 out.value(AttributeType.ID.type);
                 out.name("content");
                 out.value(((XAttributeID) value).getValue().toString());
-            }else if (value instanceof XAttributeList){
+            } else if (value instanceof XAttributeList) {
                 out.value(AttributeType.List.type);
                 out.name("content");
-                gson.toJson(((XAttributeList) value).getCollection(),xAttributeMapList,out);
-            }else if (value instanceof  XAttributeContainer){
+                gson.toJson(((XAttributeList) value).getCollection(), xAttributeMapList, out);
+            } else if (value instanceof XAttributeContainer) {
                 out.value(AttributeType.Container.type);
                 out.name("content");
-                gson.toJson(((XAttributeContainer) value).getAttributes(),xAttributeMap,out);
-            }else{
+                gson.toJson(((XAttributeContainer) value).getAttributes(), xAttributeMap, out);
+            } else {
                 throw new IOException("Unknown XAttribute type");
             }
 
