722338264132c28307e33a0756513a12d6f3a1f4
aarkue <mail@aarkue.eu>
2024-05-07 10:35:23 +0200

    Add ocel-failure.xml test
    Failed in earlier versions; likely bc of datetime format

--- process_mining/src/event_log/tests/ocel_xml_import_tests.rs
+++ process_mining/src/event_log/tests/ocel_xml_import_tests.rs
@@ -1,6 +1,6 @@
 use std::time::Instant;
 
-use crate::{import_ocel_json_from_path, import_ocel_xml_slice};
+use crate::{import_ocel_json_from_slice, import_ocel_xml_slice};
 
 #[test]
 fn test_ocel_xml_import() {
@@ -56,6 +56,21 @@ fn test_ocel_pm4py_log_json() {
     let now = Instant::now();
     let log_bytes = include_bytes!("test_data/pm4py-ocel20_example.jsonocel");
     let ocel = import_ocel_json_from_slice(log_bytes).unwrap();
+    let obj = ocel.objects.first().unwrap();
+    println!("{:?}", obj);
+    println!(
+        "Imported OCEL with {} objects and {} events in {:#?}",
+        ocel.objects.len(),
+        ocel.events.len(),
+        now.elapsed()
+    );
+}
+
+#[test]
+fn test_ocel_failing_xml() {
+    let log_bytes = include_bytes!("test_data/ocel-failure.xml");
+    let now = Instant::now();
+    let ocel = import_ocel_xml_slice(log_bytes);
     let obj = ocel.objects.first().unwrap();
     println!("{:?}", obj);
     println!(
