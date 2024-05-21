ce937ecaa05dfdbf2fc807963836ca6460395728
aarkue <mail@aarkue.eu>
2024-05-21 20:31:36 +0200

    Fix: Links in documentation

--- process_mining/src/lib.rs
+++ process_mining/src/lib.rs
@@ -51,7 +51,7 @@ pub mod petri_net {
     /// __Requires the `graphviz-export` feature to be enabled__
     ///
     /// Also requires an active graphviz installation in the PATH.
-    /// See also https://github.com/besok/graphviz-rust?tab=readme-ov-file#caveats and https://graphviz.org/download/
+    /// See also <https://github.com/besok/graphviz-rust?tab=readme-ov-file#caveats> and <https://graphviz.org/download/>
     pub mod image_export;
     /// Import [`PetriNet`] from `.pnml`
     pub mod import_pnml;
