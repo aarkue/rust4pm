65d1d9dfd4af72db81fc61a58fb98eeea2cd23dd
cRennert <christian.rennert@rwth-aachen.de>
2025-06-25 14:26:46 +0200

    Added missing documentation for the object-centric modules in lib.rs

--- process_mining/src/lib.rs
+++ process_mining/src/lib.rs
@@ -61,8 +61,11 @@ pub mod event_log {
 
 /// Object-centric discovery and conformance checking
 pub mod object_centric {
+    /// Object-centric conformance checking
     pub mod conformance;
+    /// Object-centric process trees [`OCPT`]
     pub mod ocpt;
+    /// Object-centric directly-follows graphs
     pub mod object_centric_dfg_struct;
 }
 
