92e18a113dafb33f010174c575eb223f303970a0
aarkue <git@aarkue.eu>
2025-10-27 09:50:45 +0100

    Add DuckDB Export Example

---
+++ process_mining/examples/ocel_duckdb_export.rs
@@ -0,0 +1,25 @@
+use std::{collections::HashSet, env::args, path::PathBuf};
+
+use process_mining::{import_ocel_xml_file, ocel::sql::export_ocel_duckdb_to_path};
+
+pub fn main() {
+    let path_opt = args().nth(1);
+    if let Some(mut path) = path_opt.map(PathBuf::from) {
+        let mut ocel = import_ocel_xml_file(&path);
+        // Including invalid E2O relations (i.e., to objects that do not exist) can cause corrupted or incomplete SQL exports
+        // Thus, we filter the E2O relations to only keep valid ones
+        let all_obj_ids: HashSet<_> = ocel.objects.iter().map(|o| &o.id).collect();
+        for e in &mut ocel.events {
+            e.relationships
+                .retain(|r| all_obj_ids.contains(&r.object_id));
+        }
+        // Export
+        path.set_file_name(format!(
+            "{}.duckdb",
+            path.file_name()
+                .and_then(|p| p.to_str())
+                .unwrap_or_default()
+        ));
+        export_ocel_duckdb_to_path(&ocel, &path).unwrap();
+    }
+}
