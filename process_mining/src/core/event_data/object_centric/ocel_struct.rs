2347073d3d85899c27262a3c3767584f209e81ff
aarkue <git@aarkue.eu>
2026-04-22 15:12:39 +0200

    Apply clippy fixes :)

--- process_mining/src/core/event_data/object_centric/ocel_struct.rs
+++ process_mining/src/core/event_data/object_centric/ocel_struct.rs
@@ -40,9 +40,7 @@ impl OCEL {
         let locel: IndexLinkedOCEL = IndexLinkedOCEL::from(self);
 
         let objects_with_e2o = locel
-            .e2o_rev_et
-            .iter()
-            .flat_map(|(_, o2e_set)| o2e_set.keys().cloned())
+            .e2o_rev_et.values().flat_map(|o2e_set| o2e_set.keys().cloned())
             .collect::<HashSet<_>>();
 
         let mut underlying_ocel = locel.into_inner();
